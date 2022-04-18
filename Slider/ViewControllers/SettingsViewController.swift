add59ff2c56f0270fabd61b2ebd2e12d7a1b9d58
Vasiliy Poltorak <zloyrusvelt@gmail.com>
2022-04-18 17:18:59 +0300

    Connect outlets

--- Slider/ViewControllers/SettingsViewController.swift
+++ Slider/ViewControllers/SettingsViewController.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeColor(_ sender: UISlider) {
     }
-    */
+    
+    
 
 }
