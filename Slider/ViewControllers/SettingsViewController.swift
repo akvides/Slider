c2734d783a83ce47cb41ec4aea460762b36a8e32
Vasiliy Poltorak <zloyrusvelt@gmail.com>
2022-04-18 19:34:52 +0300

    Добавил логику работы с текстовыми полями

--- Slider/ViewControllers/SettingsViewController.swift
+++ Slider/ViewControllers/SettingsViewController.swift
@@ -29,10 +29,6 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
         
         updateColor(red: red, green: green, blue: blue)
-        
-        redSlider.value = Float(red)
-        greenSlider.value = Float(green)
-        blueSlider.value = Float(blue)
 
     }
     
@@ -55,8 +51,67 @@ class SettingsViewController: UIViewController {
         greenTextField.text = String(format: "%.2f", green)
         blueTextField.text = String(format: "%.2f", blue)
         
+        redSlider.value = Float(red)
+        greenSlider.value = Float(green)
+        blueSlider.value = Float(blue)
+        
         delegate.updateColor(color.cgColor)
+        
     }
     
+    private func presentAlertController() {
+        let alertController = UIAlertController(title: "Не корректное значение",
+                                                message: "В поле можно ввести только значение от 0 до 1",
+                                                preferredStyle: .alert)
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alertController.addAction(cancelButton)
+        
+        view.endEditing(true)
+        
+        present(alertController, animated: true)
+    }
+    
+    private func changeColorFromTextFields() {
+        guard let red = redTextField.text, Float(red) != nil, Float(red)! <= 1.0 else {
+            presentAlertController()
+            redTextField.text = String(format: "%.2f", redSlider.value)
+            return
+        }
+        guard let green = greenTextField.text, Float(green) != nil, Float(green)! <= 1.0 else {
+            presentAlertController()
+            greenTextField.text = String(format: "%.2f", greenSlider.value)
+            return
+        }
+        guard let blue = blueTextField.text, Float(blue) != nil, Float(blue)! <= 1.0 else {
+            presentAlertController()
+            blueTextField.text = String(format: "%.2f", blueSlider.value)
+            return
+        }
+        
+        updateColor(red: CGFloat(Float(red)!),
+                    green: CGFloat(Float(green)!),
+                    blue: CGFloat(Float(blue)!))
+        
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 
+}
+
+extension SettingsViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        changeColorFromTextFields()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        changeColorFromTextFields()
+        view.endEditing(true)
+        return true
+    }
 }
